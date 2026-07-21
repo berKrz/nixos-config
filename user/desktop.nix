53810f7dca94e865ead4fc8c02466354eecc0dee
berKrz <bernardo@krzyuy.com.br>
2026-07-21 01:12:25 -0300

    fix: mango config syntax

--- user/desktop.nix
+++ user/desktop.nix
@@ -13,35 +13,45 @@
 				# Window Management
 				"SUPER,Return,spawn,kitty"
 				"SUPER,c,killclient,"
-				"SUPER,f,fullscreen,"
-				"SUPER_SHIFT,r,reload,"
+				"SUPER,f,togglefullscreen,"
+				"SUPER+SHIFT,r,reload_config,"
 
 				# Focus
 				"SUPER,h,focusdir,left"
-				"SUPER,j,focusdir,up"
+				"SUPER,k,focusdir,up"
 				"SUPER,j,focusdir,down"
 				"SUPER,l,focusdir,right"
 
 				# Move Windows
-				"SUPER_SHIFT,h,movewindow,left"
-				"SUPER_SHIFT,j,movewindow,up"
-				"SUPER_SHIFT,j,movewindow,down"
-				"SUPER_SHIFT,l,movewindow,right"
+				"SUPER+SHIFT,h,exchange_client,left"
+				"SUPER+SHIFT,k,exchange_client,up"
+				"SUPER+SHIFT,j,exchange_client,down"
+				"SUPER+SHIFT,l,exchange_client,right"
 
 				# Tags 1-9
-				"SUPER,1,viewtag,1"
-				"SUPER,2,viewtag,2"
-				"SUPER,3,viewtag,3"
-				"SUPER,4,viewtag,4"
-				"SUPER,5,viewtag,5"
-				"SUPER,6,viewtag,6"
-				"SUPER,7,viewtag,7"
-				"SUPER,8,viewtag,8"
-				"SUPER,9,viewtag,9"
+				"SUPER,1,tag,1"
+				"SUPER,2,tag,2"
+				"SUPER,3,tag,3"
+				"SUPER,4,tag,4"
+				"SUPER,5,tag,5"
+				"SUPER,6,tag,6"
+				"SUPER,7,tag,7"
+				"SUPER,8,tag,8"
+				"SUPER,9,tag,9"
 				
+				"SUPER+SHIFT,1,view,1"
+				"SUPER+SHIFT,2,view,2"
+				"SUPER+SHIFT,3,view,3"
+				"SUPER+SHIFT,4,view,4"
+				"SUPER+SHIFT,5,view,5"
+				"SUPER+SHIFT,6,view,6"
+				"SUPER+SHIFT,7,view,7"
+				"SUPER+SHIFT,8,view,8"
+				"SUPER+SHIFT,9,view,9"
+
 				# Noctalia IPC
 				"SUPER,space,spawn, noctalia msg panel-toggle launcher"
-				"SUPER,ESC,spawn, noctalia msg panel-toggle control-center"
+				"SUPER,Escape,spawn, noctalia msg panel-toggle control-center"
 				"SUPER,v,spawn, noctalia msg panel-toggle clipboard"
 				"SUPER,BackSpace,spawn, noctalia msg panel-toggle session"
 
